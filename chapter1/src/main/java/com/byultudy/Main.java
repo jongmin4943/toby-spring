28bf29fb10540cced0e6d52360ee767bbf4216a2
Jongmin Yoon <jongmin4943@gmail.com>
2022-11-05 22:05:07 +0900

    test : 1.1.3 UserDao test

--- chapter1/src/main/java/com/byultudy/Main.java
+++ chapter1/src/main/java/com/byultudy/Main.java
@@ -1,7 +1,28 @@
 package com.byultudy;
 
+import com.byultudy.user.dao.UserDao;
+import com.byultudy.user.domain.User;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("min");
+        user.setName("윤종민");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공");
+
     }
 }
