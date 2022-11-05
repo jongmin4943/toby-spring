a0f814bcba9f3a0e42bbde570e1a078b5631f109
Jongmin Yoon <jongmin4943@gmail.com>
2022-11-05 21:48:17 +0900

    feat : 1.1.1 User Class

---
+++ chapter1/src/main/java/com/byultudy/user/domain/User.java
@@ -0,0 +1,12 @@
+package com.byultudy.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    String id;
+    String name;
+    String password;
+}
