a54d0440bc41032a885a60fe8f4d6a55144a6af8
Jongmin Yoon <jongmin4943@gmail.com>
2022-11-07 22:17:58 +0900

    refactor : 1.4.2 UserDaoFactory connectionMaker

--- chapter1/src/main/java/com/byultudy/user/dao/DaoFactory.java
+++ chapter1/src/main/java/com/byultudy/user/dao/DaoFactory.java
@@ -2,7 +2,10 @@ package com.byultudy.user.dao;
 
 public class DaoFactory {
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        return new UserDao(connectionMaker);
+        return new UserDao(connectionMaker());
+    }
+
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
