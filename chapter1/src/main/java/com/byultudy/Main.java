b124f2221026c38ca3d04263c119c85ce75607e4
Jongmin Yoon <jongmin4943@gmail.com>
2022-11-12 10:34:55 +0900

    refactor : 2.3.1 JUnit 테스트로 변경

--- chapter1/src/main/java/com/byultudy/Main.java
+++ chapter1/src/main/java/com/byultudy/Main.java
@@ -1,38 +1,9 @@
 package com.byultudy;
 
-import com.byultudy.user.dao.DaoFactory;
-import com.byultudy.user.dao.UserDao;
-import com.byultudy.user.domain.User;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-
-import java.sql.SQLException;
+import org.junit.runner.JUnitCore;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user = new User();
-        user.setId("min");
-        user.setName("윤종민");
-        user.setPassword("1234");
-
-        dao.add(user);
-
-        System.out.println(user.getId() + "등록 성공");
-
-        User user2 = dao.get(user.getId());
-        if(!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
-
+    public static void main(String[] args) {
+//        JUnitCore.main("com.byultudy.user.dao.UserDaoTest");
     }
 }
---
+++ chapter1/src/test/java/com/byultudy/user/dao/UserDaoTest.java
@@ -0,0 +1,38 @@
+package com.byultudy.user.dao;
+
+import com.byultudy.user.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class UserDaoTest {
+
+    @Test
+    public void addAndGet() throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("min");
+        user.setName("윤종민");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
+
+    }
+
+}
