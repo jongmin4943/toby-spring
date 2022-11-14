478157d093b17fd55b7cd4f1a4c49ecd55b9bb0a
Jongmin Yoon <jongmin4943@gmail.com>
2022-11-15 01:24:27 +0900

    feat : 2.4.1 어플리케이션 컨텍스트
    - 컨텍스트 관련 어노테이션 추가
    - junit 4 를 위한 junit-vintage-engine 추가

--- chapter1/src/test/java/com/byultudy/user/dao/UserDaoTest.java
+++ chapter1/src/test/java/com/byultudy/user/dao/UserDaoTest.java
@@ -1,11 +1,14 @@
 package com.byultudy.user.dao;
 
 import com.byultudy.user.domain.User;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
@@ -13,15 +16,18 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertThrows;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
+    @Autowired
+    private ApplicationContext context;
     private UserDao dao;
     private User user1;
     private User user2;
     private User user3;
-    @BeforeEach
-    void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        this.dao = context.getBean("userDao", UserDao.class);
+    @Before
+    public void setUp() {
+        this.dao = this.context.getBean("userDao", UserDao.class);
         this.user1 = new User("test1", "테스터1", "test");
         this.user2 = new User("test2", "테스터2", "test");
         this.user3 = new User("test3", "테스터3", "test");
