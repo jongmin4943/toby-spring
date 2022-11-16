807ceac62d2b70f162a4f72326e998041b02cc7f
Jongmin Yoon <jongmin4943@gmail.com>
2022-11-16 22:05:54 +0900

    docs: 2장 마무리
    - 프로젝트 정리

--- chapter1/src/main/java/com/byultudy/Main.java
+++ chapter1/src/main/java/com/byultudy/Main.java
@@ -1,9 +1,31 @@
 package com.byultudy;
 
-import org.junit.runner.JUnitCore;
+import com.byultudy.user.dao.UserDao;
+import com.byultudy.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-//        JUnitCore.main("com.byultudy.user.dao.UserDaoTest");
+    public static void main(String[] args) throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("min");
+        user.setName("윤종민");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공");
+
     }
 }
