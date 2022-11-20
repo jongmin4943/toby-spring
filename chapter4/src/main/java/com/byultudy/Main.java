19d6125027710e545c589840aba30b6a5b5479a1
Jongmin Yoon <jongmin4943@gmail.com>
2022-11-21 01:05:11 +0900

    feat : 4.1.4 예외처리 전략
    - UserDao add 메서드 중복 Id 관련 예외처리 추가

---
+++ chapter4/src/main/java/com/byultudy/Main.java
@@ -0,0 +1,7 @@
+package com.byultudy;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ chapter4/src/test/java/com/byultudy/user/dao/UserDaoTest.java
@@ -0,0 +1,103 @@
+package com.byultudy.user.dao;
+
+import com.byultudy.user.domain.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThrows;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
+public class UserDaoTest {
+    @Autowired
+    private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
+    @Before
+    public void setUp() {
+        this.user1 = new User("test1", "테스터1", "test");
+        this.user2 = new User("test2", "테스터2", "test");
+        this.user3 = new User("test3", "테스터3", "test");
+    }
+
+    @Test
+    public void addAndGet() {
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
+
+    }
+
+    @Test
+    public void getUserFailure() {
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+        assertThrows(EmptyResultDataAccessException.class, ()->
+            dao.get("unknown")
+        );
+    }
+
+    @Test
+    public void count() {
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+    }
+
+    @Test
+    public void getAll() {
+        dao.deleteAll();
+        List<User> users = dao.getAll();
+        assertThat(users.size(), is(0));
+
+        dao.add(user1);
+        List<User> users1 = dao.getAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+
+        dao.add(user2);
+        List<User> users2 = dao.getAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+
+        dao.add(user3);
+        List<User> users3 = dao.getAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user1, users3.get(0));
+        checkSameUser(user2, users3.get(1));
+        checkSameUser(user3, users3.get(2));
+    }
+
+    private void checkSameUser(final User user1, final User user2) {
+        assertThat(user1.getId(), is(user2.getId()));
+        assertThat(user1.getName(), is(user2.getName()));
+        assertThat(user1.getPassword(), is(user2.getPassword()));
+    }
+}
