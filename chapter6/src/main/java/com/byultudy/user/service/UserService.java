ddc0997cf6fb237bd79e53db689e8e0dbdfc6731
Jongmin Yoon <jongmin4943@gmail.com>
2022-12-16 02:09:19 +0900

    feat : 6.7.1 트랜잭션 어노테이션
    - 어노테이션을 이용한 트랜잭션 설정

--- chapter6/src/main/java/com/byultudy/user/service/UserService.java
+++ chapter6/src/main/java/com/byultudy/user/service/UserService.java
@@ -1,14 +1,18 @@
 package com.byultudy.user.service;
 
 import com.byultudy.user.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void upgradeLevels();
 
     void add(User user);
+    @Transactional(readOnly = true)
     User get(String id);
+    @Transactional(readOnly = true)
     List<User> getAll();
     void deleteAll();
     void update(User user);
