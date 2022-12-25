2fdd4376878ed55e84d439aa32abff31cef959b5
Jongmin Yoon <jongmin4943@gmail.com>
2022-12-26 01:32:17 +0900

    feat : 7.4.2 인터페이스 상속
    - UpdatableSqlRegistry 생성

---
+++ chapter7/src/main/java/com/byultudy/user/sqlService/SqlUpdateFailureException.java
@@ -0,0 +1,19 @@
+package com.byultudy.user.sqlService;
+
+public class SqlUpdateFailureException extends RuntimeException {
+    public SqlUpdateFailureException() {
+        super();
+    }
+
+    public SqlUpdateFailureException(String message) {
+        super(message);
+    }
+
+    public SqlUpdateFailureException(Throwable cause) {
+        super(cause);
+    }
+
+    public SqlUpdateFailureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ chapter7/src/main/java/com/byultudy/user/sqlService/UpdatableSqlRegistry.java
@@ -0,0 +1,9 @@
+package com.byultudy.user.sqlService;
+
+import java.util.Map;
+
+public interface UpdatableSqlRegistry extends SqlRegistry {
+    void updateSql(String key, String sql) throws SqlUpdateFailureException;
+
+    void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException;
+}
